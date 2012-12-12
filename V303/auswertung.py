97160012063343e7ba46a8bd82fcbecced0f93d9
Daniel Meißner <daniel.meissner@udo.edu>
2012-12-12 19:30:48 +0100

    [V303] Auswertung. 1. Teil fertig, 2. Teil angefangen

--- V303/auswertung.py
+++ V303/auswertung.py
@@ -4,17 +4,29 @@
 # Attribution-NonCommercial 3.0 Unported License. To view a copy of this
 # license, visit http://creativecommons.org/licenses/by-nc/3.0/.  
 
+import matplotlib
+
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
-def auswertung_winkel_spannung(phi, U_out, p0=None):
-    (A, p), cov = opt.curve_fit(lambda x, A, p: A*np.cos(x+p), phi, U_out, p0)
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.unicode'] = True
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['font.serif'] = ['Computer Modern Roman']
+
+def auswertung_winkel_spannung(phi, U_out, title, filename):
+    (A, p), cov = opt.curve_fit(lambda x, A, p: A*np.cos(x+p), phi, U_out)
 
     x = np.linspace(0, 2*np.pi)
+
+    plt.title(title)
+    plt.xlabel("$\phi$")
+    plt.ylabel(r"$U_\text{out}/\mathrm{V}$")
     plt.plot(phi, U_out, '+')
     plt.plot(x, A*np.cos(x+p))
-    plt.show()
+    plt.savefig(filename)
+    plt.close()
 
     print u"U_out = ({0:.5f}±{1:.5f} V)*cos(x+{2:.5f})"\
         .format(A, np.sqrt(cov[0][0]), p)
@@ -23,10 +35,23 @@ ohne_rauschen = np.loadtxt('ohne-rauschen.txt', unpack=True)
 phi = np.deg2rad(ohne_rauschen[0])
 U_out = ohne_rauschen[1]/1000.
 
-auswertung_winkel_spannung(phi, U_out)
+auswertung_winkel_spannung(phi, U_out, "Ohne Rauschen", "ohne-rauschen.pdf")
 
 mit_rauschen = np.loadtxt('mit-rauschen.txt', unpack=True)
 phi = np.deg2rad(mit_rauschen[0])
 U_out = mit_rauschen[1]/1000.
 
-auswertung_winkel_spannung(phi, U_out)
+auswertung_winkel_spannung(phi, U_out, "Mit Rauschen", "mit-rauschen.pdf")
+
+lichtmessung = np.loadtxt('messung-mit-licht.txt', unpack=True)
+a = lichtmessung[0]/100 # Abstand in m
+U = lichtmessung[1]/lichtmessung[2]/1000 # Spannung in V
+
+plt.title("Die Lichtrauschmessung")
+plt.xlabel("Abstand in Metern")
+plt.ylabel("Spannung in Volt")
+plt.plot(a, U, "+")
+
+(A, B), cov = opt.curve_fit(lambda x, A, B: A/x**2 + B, a, U)
+plt.plot(a, A/a**2 + B)
+plt.show()
