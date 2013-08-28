37dafba6df3cac8297c7ae7b793dbf7b254e15a5
Daniel Meißner <daniel.meissner@udo.edu>
2013-08-28 23:08:28 +0200

    [V103] Überarbeitung des Auswertungsskripts
    - Auswertungsskript für Python 3 angepaßt
    - Refactoring der Ausgabe
    - UTF-8 kodierte Zeichen aus den Datenquellen entfernt

--- V103/auswertung.py
+++ V103/auswertung.py
@@ -4,10 +4,6 @@
 # Attribution-NonCommercial 3.0 Unported License. To view a copy of this
 # license, visit http://creativecommons.org/licenses/by-nc/3.0/.  
 
-from __future__ import unicode_literals
-from __future__ import division
-
-
 import codecs
 import sys
 import math
@@ -16,9 +12,6 @@ import matplotlib
 from numpy import loadtxt, array_split, delete
 from matplotlib import pyplot as plt
 import linregress
-
-UTF8Writer = codecs.getwriter('utf8')
-sys.stdout = UTF8Writer(sys.stdout)
 
 matplotlib.rcParams['text.usetex'] = True
 matplotlib.rcParams['text.latex.unicode'] = True
@@ -32,7 +25,7 @@ def auswertung_einseitig(titel, datei, x, D, d, L, m, I, entf_x):
     u = L*x**2 - x**3/3
     entf_u = delete(u, entf_x)
     entf_D = delete(D, entf_x)
-    (A, B), (dA, dB) = linregress.linear_fit(entf_u, entf_D)
+    A, dA, B, dB = linregress.linear_fit(entf_u, entf_D)
 
     E  = (m*g)/(2*A*I)
     dE = (m*g*dA)/(2*A**2*I)
@@ -41,23 +34,23 @@ def auswertung_einseitig(titel, datei, x, D, d, L, m, I, entf_x):
     plt.title(titel)
     plt.xlabel(r"$(Lx^2 - \frac{x^3}{3})/\mathrm{m}^3$")
     plt.ylabel(r"$D/\mathrm{m}$")
+    plt.plot(entf_u, entf_D, "r+")
+    plt.plot(entf_u, A*entf_u+B)
     if len(u.take(entf_x)) != 0:
         plt.plot(u.take(entf_x), D.take(entf_x), "+", 
                  label="ausgeschlossene Werte")
-    plt.plot(entf_u, entf_D, "r+")
-    plt.plot(entf_u, A*entf_u+B)
-    plt.legend()
+
+        plt.legend()
     plt.savefig(datei)
     plt.clf()
 
-    print titel
-    print 76*'-'
-    print u"Durchmesser d = ({0:.3f} ± {1:.3f}) mm"\
-        .format(d.mean()*1e3, d.std(ddof=1)*1e3)
-    print u"Elastizitätsmodul E = ({0:.3f} ± {1:.3f}) kN/mm^2"\
-        .format(E*1e-9, dE*1e-9)
-    print u"Steigung: A={0:.3f}±{1:.4f}".format(A, dA)
-    print 76*'-'
+    print(titel + """
+  ------------------------------------------------------------------------
+  Durchmesser d                     ({0:.5e} ± {1:.5e})  mm
+  Elastizitätsmodul E               ({2:.5e} ± {3:.5e})  kN/mm^2
+  Steigung: A                        {4:.5e} ± {5:.5e}
+  ------------------------------------------------------------------------
+    """.format(d.mean()*1e3, d.std(ddof=1)*1e3, E*1e-9, dE*1e-9, A, dA))
 
 def auswertung_beidseitig(titel, datei, x, D, d, L, m, I):
     x1, x2 = array_split(x, 2)
@@ -66,8 +59,8 @@ def auswertung_beidseitig(titel, datei, x, D, d, L, m, I):
     u1 = 3*L**2*x1 - 4*x1**3
     u2 = 4*x2**3 - 12*L*x2**2 + 9*L**2*x2 - L**3
 
-    (A1, B1), (dA1, dB1) = linregress.linear_fit(u1, D1)
-    (A2, B2), (dA2, dB2) = linregress.linear_fit(u2, D2)
+    A1, dA1, B1, dB1 = linregress.linear_fit(u1, D1)
+    A2, dA2, B2, dB2 = linregress.linear_fit(u2, D2)
 
     E1  = (m*g)/(48*A1*I)
     E2  = (m*g)/(48*A2*I)
@@ -93,27 +86,26 @@ def auswertung_beidseitig(titel, datei, x, D, d, L, m, I):
 
     E = (E1+E2)/2
 
-    print titel
-    print 76*'-'
-    print u"Durchmesser d = ({0:.3f} ± {1:.3f}) mm"\
-        .format(d.mean()*1e3, d.std(ddof=1)*1e3)
-    print u"Elastizitätsmodul E1 = ({0:.3f} ± {1:.3f}) kN/mm^2"\
-        .format(E1*1e-9, dE1*1e-9)
-    print u"Elastizitätsmodul E2 = ({0:.3f} ± {1:.3f}) kN/mm^2"\
-        .format(E2*1e-9, dE2*1e-9)
-    print u"Steigungen: A_f={0:.4f}±{1:.4f}, A_g={2:.4f}±{3:.4f}"\
-        .format(A1, dA1, A2, dA2)
-    print u"intersection: B_f={0:.4f}, B_g={1:.4f}"\
-        .format(B1, B2)
-    print E
-    print 76*'-'
+    print(titel + """
+  ------------------------------------------------------------------------
+  Durchmesser d                     ({0:.5e} ± {1:.5e})  mm
+  Elastizitätsmodul E_f             ({2:.5e} ± {3:.5e})  kN/mm^2
+  Elastizitätsmodul E_g             ({4:.5e} ± {5:.5e})  kN/mm^2
+  Steigung A_f                       {6:.5e} ± {7:.5e}
+  Steigung A_g                       {8:.5e} ± {9:.5e}
+  Ordinatenabschnitt B_f             {10:.5e}
+  Ordinatenabschnitt B_f             {11:.5e}
+  ------------------------------------------------------------------------
+  E-Modul (Mittelwert)               {12:.5e}  kN/mm^2
+    """.format(d.mean()*1e3, d.std(ddof=1)*1e3, E1*1e-9, dE1*1e-9, E2*1e-9,
+               dE2*1e-9, A1, dA1, A2, dA2, B1, B2, E))
 
 ############################################################################
 # Stahl einseitig eingespannt, kreisförmiger Querschnitt
 
 # Daten laden
-x, D_v, D_n = loadtxt('stahl-einseitig-kreisfoermig.txt', unpack=True)
-d = loadtxt('stahl-kreisfoermig-durchmesser.txt', unpack=True)
+x, D_v, D_n = loadtxt("stahl-einseitig-kreisfoermig.txt", unpack=True)
+d = loadtxt("stahl-kreisfoermig-durchmesser.txt", unpack=True)
 
 # Umrechnen auf Meter
 x *= 1e-2
@@ -124,7 +116,7 @@ D = (D_n - D_v)*1e-6
 I = d.mean()**4*math.pi/64
 
 # Auswertung beginnen
-auswertung_einseitig(u"Stahl einseitig eingespannt, kreisförmiger Querschnitt",\
+auswertung_einseitig("Stahl einseitig eingespannt, kreisförmiger Querschnitt",\
                          "stahl-einseitig-kreisfoermig.pdf",\
                          x, D, d, L = 0.495, m = 1.6809, I=I,\
                          entf_x = range(0,4)) # ersten vier Werte rausnehmen
@@ -145,7 +137,7 @@ D = (D_n - D_v)*1e-6
 I = d.mean()**4/12
 
 # Auswertung beginnen
-auswertung_einseitig(u"Aluminium einseitig eingespannt, quadratischer "\
+auswertung_einseitig("Aluminium einseitig eingespannt, quadratischer "\
                          "Querschnitt", "aluminium-einseitig-quadratisch.pdf",\
                          x, D, d, L = 0.496, m = 0.5416, I=I, entf_x=[])
 
@@ -164,6 +156,6 @@ D = (D_n - D_v)*1e-6
 # Flächenträgheitsmoment
 I = d.mean()**4/12
 
-auswertung_beidseitig(u"Messing beidseitig eingespannt, quadratischer "\
+auswertung_beidseitig("Messing beidseitig eingespannt, quadratischer "\
                           "Querschnitt", "messing-beidseitig-quadratisch.pdf",\
                           x, D, d, L=0.551, m=4.1, I=I)
