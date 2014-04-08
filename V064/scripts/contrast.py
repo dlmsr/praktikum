521e58fc66fa68c6fd966e78cf33d147271886ed
Daniel Meißner <daniel.meissner@udo.edu>
2014-04-08 07:19:39 +0200

    [V064] Auswertung abgeschlossen.
    Die Auswertungsskripte berechnen alle notwendigen Daten.  In der Datei
    tex/anhang-rohdaten.tex wurden die Meßwertetabellen hinzugefügt.  Es fehlen
    nur noch die Daten zu den Gas-Messungen.
    
    Die Datei tex/auswertung.tex enthält soweit alles notwendige.  In der Theorie
    tex/theorie.tex wurden noch zwei Label angepaßt.

--- V064/scripts/contrast.py
+++ V064/scripts/contrast.py
@@ -1,5 +1,9 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+#
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial 3.0 Unported License. To view a copy of this
+# license, visit http://creativecommons.org/licenses/by-nc/3.0/.
 
 from __future__ import unicode_literals
 
@@ -13,13 +17,36 @@ def f_max(phi, A, B, C):
 def f_min(phi, A, B, C):
     return A * (1 - np.sin(2 * phi + B)) + C
 
-# printing settings
-np.set_printoptions(precision=4)
+def cov2std(cov):
+    """calculate relative standard deviations of parameter estimates
+
+    """
+    std = np.zeros(len(cov))
+    for i in np.arange(len(cov)):
+        std[i] = cov[i, i]
+    std = np.sqrt(std)
+    return std
+
 
 phi, I_max, I_min = np.loadtxt("../data/contrast").T
 
 # convert angles from degrees to radians
 phi = np.radians(phi)
+
+# calculate contrast and plot data
+V = (I_max - I_min)/(I_max + I_min)
+x = np.linspace(0, np.pi)
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, np.pi)
+ax.set_xlabel("Polarisationswinkel im Bogenmaß")
+ax.set_ylabel("Kontrast")
+plots = ax.plot(phi, V, "o", x, np.sin(2*x))
+texts = ["gemessener Kontrast", "$\sin(2x)$"]
+plt.legend(plots, texts)
+fig.tight_layout()
+fig.show()
+fig.savefig("../figures/contrast.pdf")
 
 # generate figure with two axes in a row that share the x axis
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False)
@@ -39,20 +66,24 @@ popt[1,:], pcov[1,:,:] = opt.curve_fit(f_min, phi, I_min)
 # generate equally distributed angles between phi_max and
 # phi_min, plot data and fitted curves
 alpha = np.linspace(phi.min(), phi.max())
-ax1.plot(phi, I_max, "x")
-ax1.plot(alpha, f_max(alpha, popt[0,0], popt[0,1], popt[0,2]))
-ax2.plot(phi, I_min, "x")
-ax2.plot(alpha, f_min(alpha, popt[1,0], popt[1,1], popt[1,2]))
+i_max = f_max(alpha, popt[0,0], popt[0,1], popt[0,2])
+i_min = f_min(alpha, popt[1,0], popt[1,1], popt[1,2])
+
+angles = np.array([phi, alpha, phi, alpha])
+intens = np.array([I_max, i_max, I_min, i_min])
+
+ax1.plot(phi, I_max, "o")
+ax1.plot(alpha, i_max)
+ax2.plot(phi, I_min, "o")
+ax2.plot(alpha, i_min)
 
 fig.tight_layout()
-fig.savefig("../figures/contrast.pdf")
+fig.savefig("../figures/intensity.pdf")
+fig.show()
 
-# calculate relative standard deviations of parameter estimates
-std = np.zeros((2, 3))
-for i in np.arange(3):
-    std[:, i] = pcov[:, i, i]
-std = np.sqrt(std)
-relstd = std/np.abs(popt)
+
+# printing settings
+np.set_printoptions(precision=4)
 
 print("""
 The estimated parameters in volts
@@ -61,6 +92,12 @@ max. intens.   A   B   C
 min. intens.   A   B   C
 """)
 print(popt)
+
+std = np.zeros((2,3))
+for i in np.arange(2):
+    std[i,:] = cov2std(pcov[i,:,:])
+
+relstd = std/np.abs(popt)
 
 print("""
 The relative standard deviations of the estimated parameters
--- V064/scripts/refraction.py
+++ V064/scripts/refraction.py
@@ -1,47 +1,94 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+#
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial 3.0 Unported License. To view a copy of this
+# license, visit http://creativecommons.org/licenses/by-nc/3.0/.
 
 from __future__ import unicode_literals
 from __future__ import division
 
 import numpy as np
+from numpy import array, loadtxt, radians, linspace, zeros, mean, var
+from numpy import ma
+from matplotlib import pyplot as plt
+from scipy import stats
 
 
-def calc_refraction_index_slab(thickness, vac_wavelen, counts,
-                               phi_1, phi_2):
+# printing settings
+np.set_printoptions(precision=5)
 
-    ref_index = 1/(1 - counts * vacuum_wavelength /
-                   (thickness * (phi_2**2 - phi_1**2)))
+# load and prepare technical data
+vac_wavelen, thickness, delta_phi, phi_1, length = loadtxt("../data/technical")
 
-    return np.array([ref_index.mean(), ref_index.std(ddof = 1)])
+# convert lengths to nanometers
+thickness = thickness * 1e6
+length    = length * 1e6
 
+# convert angles to radians
+delta_phi = radians(delta_phi)
+phi_1     = radians(phi_1)
 
-def calc_refraction_index_gas(length, vac_wavelen, counts):
+########################################################################
+# load data for glass measurement
+counts = loadtxt("../data/refraction/glass")
 
-    ref_index = 0
+# delta phi = phi_2 - phi_1
+phi_2  = phi_1 + delta_phi
 
-    return ref_index
+# calculate refraction indices and mean
+ref_index = 1/(1 - counts * vac_wavelen /
+               (thickness * (phi_2**2 - phi_1**2)))
 
+print("Refraction index of glass slabs:   {:.4f} ± {:.4f}"
+      .format(ref_index.mean(), ref_index.std(ddof = 1)))
 
-if __name__ == "__main__":
+########################################################################
+# load and prepare data for gas measurement
+pressure = zeros((11, 3))
+counts   = zeros((11, 3))
 
-    # printing settings
-    np.set_printoptions(precision=4)
+pressure[:,0], counts[:,0] = loadtxt("../data/refraction/air-1", unpack=True)
+pressure[:,1], counts[:,1] = loadtxt("../data/refraction/air-2", unpack=True)
+pressure[:,2], counts[:,2] = loadtxt("../data/refraction/co2", unpack=True)
 
-    # load and prepare data
-    data_glass        = np.loadtxt("../data/refraction/glass")
-    vacuum_wavelength = data_glass[0]  # nm
-    thickness         = data_glass[1]  # nm
-    delta_phi         = np.radians(data_glass[2])
-    phi_1             = np.radians(data_glass[3])
-    counts            = data_glass[4:]
+pressure = ma.masked_invalid(pressure)
+counts   = ma.masked_invalid(counts)
 
-    # delta phi = phi_2 - phi_1
-    phi_2  = phi_1 + delta_phi
+# print result and plot pressure vs refraction index
+ref_index = counts * vac_wavelen / length + 1
 
-    print(calc_refraction_index_slab(thickness, vacuum_wavelength,
-                                     counts, phi_1, phi_2))
+fig, ax = plt.subplots()
+ax.set_xlabel("Druck in Millibar")
+ax.set_ylabel("Brechungsindex")
 
-    pressure, counts = np.loadtxt("../data/refraction/air-1").T
-    pressure, counts = np.loadtxt("../data/refraction/air-2").T
-    pressure, counts = np.loadtxt("../data/refraction/co2").T
+data_points = ax.plot(pressure, ref_index, "o")
+texts       = ["Luft Messung 1", "Luft Messung 2", "CO$_2$ Messung"]
+plt.legend(data_points, texts, loc=4)
+
+for i in range(3):
+
+    # skip masked entries
+    x = ma.compressed(pressure[:,i])
+    y = ma.compressed(ref_index[:,i])
+
+    beta0, beta1, r_value, p_value, std_err = stats.linregress(x, y)
+
+    x = linspace(0, 1600)
+    y = beta0 * x + beta1
+    ax.plot(x, y)
+
+    # calculate standard deviations of parameters
+    n = len(x)
+    s_beta0 = std_err * np.sqrt(1/n * (1 + mean(x)**2/var(x)))
+    s_beta1 = std_err * np.sqrt(1/n * 1/var(x))
+
+    print(array([[beta0, s_beta0], [beta1, s_beta1]]))
+    print(array([beta0*1000 + beta1,
+                 np.sqrt(1000**2*s_beta0**2 + s_beta1**2)]))
+
+fig.savefig("../figures/pressure-ref-index.pdf")
+
+result = np.array([pressure[:,0], ref_index[:,0], pressure[:,1],
+                   ref_index[:,1], pressure[:,2], ref_index[:,2]]).T
+print(result)
