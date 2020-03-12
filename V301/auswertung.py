5423333ff4de0599f76b28669ea02f185da41adf
Daniel Meißner <daniel.meissner-i4k@ruhr-uni-bochum.de>
2020-03-12 12:10:24 +0100

    [V301] Überarbeitung des Auswertungsskripts
    - scipy.stats.linregress für lineare Regression
    - verhindere “pollution of global namespace”
    - erzeuge PDFs, die nicht mehr skaliert werden
    - Regressionsplots in eine Abbildung

--- V301/auswertung.py
+++ V301/auswertung.py
@@ -1,11 +1,16 @@
 import numpy as np
-from numpy import *
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
 from scipy import stats
 
-(Rg,Ig,Ug,Rr,Ir,Ur,Rs,Is,Us) = loadtxt("a_b_und_d.txt",unpack = True)
-(Rc,Ic,Uc) = loadtxt("c.txt",unpack = True)
+plt.style.use("mypubstyle")
+plt.rcParams.update(**{
+    "pgf.preamble": [r"\usepackage{siunitx}"],
+    "text.latex.preamble": [r"\usepackage{siunitx}"],
+})
+
+(Rg,Ig,Ug,Rr,Ir,Ur,Rs,Is,Us) = np.loadtxt("a_b_und_d.txt",unpack = True)
+(Rc,Ic,Uc) = np.loadtxt("c.txt",unpack = True)
 #Alles in SI-Einheiten umrechnen
 Rg = Rg*0.5 #Ohm
 Ig = Ig #Ampere
@@ -23,10 +28,12 @@ Rc = Rc*0.5 #Ohm
 Ic = Ic*10**(-3)#Ampere
 Uc = Uc#Volt
 
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(5.78, 2.2),
+                                    squeeze=True)
 
 #1: U_K = f(I) für die Monozelle zeichnen
 
-plot(Ig,Ug,"r*",label="Gleichspannung")
+ax1.plot(Ig,Ug,"r*",label="Gleichspannung")
 
 
 
@@ -44,45 +51,41 @@ dys = dms * np.sqrt(1/(Is.size) * np.sum(Is**2))
 def f(x,A,B):
     return A*x+B
 
-xg= linspace(0,0.12,2)
-plot(xg,f(xg,mg,yg),"r")
+xg= np.linspace(0,0.12,2)
+ax1.plot(xg,f(xg,mg,yg),"r")
 
 
 #Außerdem: Plot mit Gegenspannung
 
-plot(Ic,Uc,"b*",label="Gegenspannung")
+ax1.plot(Ic,Uc,"b*",label="Gegenspannung")
 mc, yc, _, _, dmc = stats.linregress(Ic, Uc)
 dyc = dmc * np.sqrt(1/(Ic.size) * np.sum(Ic**2))
 
-xc = linspace(0,0.12,2)
-plot(xc,f(xc,mc,yc),"b")
-xlabel("I / A")
-ylabel("$U_K$ / V")
-grid()
-legend(loc="center right")
-savefig("mono.pdf")
-close()
+xc = np.linspace(0,0.12,2)
+ax1.plot(xc,f(xc,mc,yc),"b")
+ax1.set_xlabel(r"$I/\si{\ampere}$")
+ax1.set_ylabel(r"$U_K/\si{\volt}$")
+ax1.set_title("Monozelle")
+ax1.legend(loc="upper left", fontsize=6)
+
 
 # U_K = f(I) für s und r zeichnen
-xr=linspace(0,0.01,2)
-plot(Ir,Ur,"k*",label="Messwerte(Rechteck)")
-plot(xr,f(xr,mr,yr),"k",label="Ausgleichsgerade")
-xlabel("I / A")
-ylabel("$U_K$ / V")
-grid()
-legend(loc="upper right")
-savefig("rechteck.pdf")
-close()
+ax2.set_title("Rechteck")
+ax2.set_xlabel(r"$I/\si{\ampere}$")
+xr=np.linspace(0,0.01,2)
+ax2.plot(Ir,Ur,"k*")
+ax2.plot(xr,f(xr,mr,yr),"k")
 
-xs=linspace(0,0.002,2)
-plot(Is,Us,"b*",label="Messwerte(Sinus)")
-plot(xs,f(xs,ms,ys),"b",label="Ausgleichsgerade")
-xlabel("I / A")
-ylabel("$U_K$ / V")
-grid()
-legend(loc="upper right")
-savefig("sinus.pdf")
-close()
+
+ax3.set_title("Sinus")
+ax3.set_xlabel(r"$I/\si{\ampere}$")
+xs=np.linspace(0,0.002,2)
+ax3.plot(Is,Us,"k*")
+ax3.plot(xs,f(xs,ms,ys),"k")
+
+
+plt.tight_layout(pad=0.25)
+fig.savefig("linregress.pdf")
 
 print("Spannungsquelle --- Innenwiderstand --- Leerlaufspannung")
 print("Gleichsp. --- {} +- {} Ohm --- {} +- {} Volt".format(-mg,dmg,yg,dyg))
@@ -116,7 +119,7 @@ Ud_ber = -mg # Errechnete Leerlaufspannung
 Ri = Rges # Errechneter Innenwiderstand der Spannungsquelle
 
 DUd_array = Ri*Ug/(R_v) 
-DUd = mean(DUd_array)
+DUd = np.mean(DUd_array)
 print("-------------------------------------")
 print("Systematischer Fehler der direkten Messung = {}Volt".format(DUd))
 
@@ -126,16 +129,19 @@ print("Systematischer Fehler der direkten Messung = {}Volt".format(DUd))
 def N(ra):
     return (U_0ges - Rges*U_0ges/(Rges + ra))**2/ra
 
-linx = linspace(-0.1,52,1000)
-plot(linx,N(linx),label = "Theoriekurve")
+linx = np.linspace(-0.1,52,1000)
+
+plt.figure(figsize=(5.78, 2.89))
+plt.plot(linx, N(linx), color="black", label="Theoriekurve")
 #Messwerte U*I gegen Ra
-plot(Rg,Ug*Ig,"*", label = "Messwerte")
-xlabel("$R_a$ in Ohm")
-ylabel("N in Watt")
-grid()
-legend(loc = "center right")
-savefig("leistung.pdf")
-close()
+plt.plot(Rg,Ug*Ig, "k*", label="Messwerte")
+plt.xlabel("$R_a$ in Ohm")
+plt.ylabel("N in Watt")
+plt.grid()
+plt.legend(loc="lower right")
+plt.tight_layout()
+plt.savefig("leistung.pdf")
+plt.close()
 
 print("----Leistungsbetrachtung----")
 print("Theorie und Experiment stimmen überein")
